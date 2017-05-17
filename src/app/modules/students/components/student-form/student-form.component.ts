1e46ca6815ee1ba05e4bf94a35c3545e2ea50a47
ksenkso <yazunya@yandex.ru>
2017-05-17 20:57:36 +0300

    Из формы удалены дублирующиеся заголовки. Изменён StudentsService для работы с новым API

--- src/app/modules/students/components/student-form/student-form.component.ts
+++ src/app/modules/students/components/student-form/student-form.component.ts
@@ -5,26 +5,7 @@ import {Group} from "../../../../interfaces/group";
 import {StudentsService} from "../../services/students.service";
 import {Router, ActivatedRoute, Params, } from "@angular/router";
 import {Location} from "@angular/common";
-import {isUndefined} from "util";
 
-const groups: Group[] = [
-  {
-    group_id: 1,
-    group_name: 'ПКС.13А'
-  },
-  {
-    group_id: 2,
-    group_name: 'ПКС.14А'
-  },
-  {
-    group_id: 3,
-    group_name: 'ПКС.15А'
-  },
-  {
-    group_id: 4,
-    group_name: 'ПКС.16А'
-  },
-];
 
 @Component({
   selector: 'app-student-form',
@@ -36,12 +17,11 @@ export class StudentFormComponent implements OnInit, OnChanges {
   @Input() mode: string;
   @Input() student: Student;
   studentForm: FormGroup;
-  groups = groups;
+  groups: Group[] = [];
 
   constructor(
     private fb: FormBuilder,
     private studentsService: StudentsService,
-    private router: Router,
     private route: ActivatedRoute,
     private location: Location
   ) {
@@ -60,7 +40,7 @@ export class StudentFormComponent implements OnInit, OnChanges {
     })
   }
 
-  log(data) {
+  static log(data) {
     console.log(data);
   }
 
@@ -69,13 +49,13 @@ export class StudentFormComponent implements OnInit, OnChanges {
 
     if (this.mode === 'create') {
       this.studentsService.createStudent(this.student)
-        .then(this.log.bind(this))
+        .then(StudentFormComponent.log.bind(this))
         .then(this.revert.bind(this))
         .then(this.goBack.bind(this))
     } else {
       console.log('submit', this.student);
       this.studentsService.saveStudent(this.student)
-        .then(this.log.bind(this))
+        .then(StudentFormComponent.log.bind(this))
         .then(this.revert.bind(this))
         .then(this.goBack.bind(this))
     }
@@ -92,26 +72,22 @@ export class StudentFormComponent implements OnInit, OnChanges {
   prepareSaveStudent(): Student {
     const formModel = this.studentForm.value;
 
-    const saveStudent: Student = {
+    return <Student>{
       student_id: this.student.student_id,
       first_name: formModel.first_name as string,
       last_name: formModel.last_name as string,
       patronymic: formModel.patronymic as string,
       address: formModel.address as string,
-      //date: formModel.date as string,
       phone: formModel.phone as string,
-      //email: formModel.email as string,
-      //login: formModel.login as string,
       group_id: formModel.group_id as string,
     };
-    return saveStudent;
   }
 
 
 
 
   ngOnChanges() {
-    this.log(this.student);
+    StudentFormComponent.log(this.student);
     if (!this.student) {
       this.student = <Student> {
         first_name: '',
@@ -134,6 +110,11 @@ export class StudentFormComponent implements OnInit, OnChanges {
   }
 
   ngOnInit() {
+
+    this.studentsService
+      .getGroups()
+      .then(groups => this.groups = groups);
+
     if (this.mode === 'update') {
       this.route.params
         .switchMap((params: Params) => this.studentsService.getStudent(+params['id']))
--- src/app/modules/students/services/students.service.ts
+++ src/app/modules/students/services/students.service.ts
@@ -1,48 +1,52 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import {Student} from "../interfaces/student";
+import {ApiClientService} from "../../shared/services/api-client.service";
+import {Group} from "../../../interfaces/group";
 
 @Injectable()
 export class StudentsService {
 
-  private endpoint: string = '/api/students';
+  private endpoint: string = 'http://api.journal.ru/v1/student';
 
-  constructor(private http: Http) {
+  constructor(
+    private http: Http,
+    private apiClient: ApiClientService
+  ) {
 
   }
 
   getStudent(id: string|number): Promise<Student> {
-    return this.http.get(`${this.endpoint}/${id}`)
-      .toPromise()
-      .then(response => response.json() as Student)
-      .catch(this.handleError);
+
+    return this.apiClient
+      .get<Student>(`${this.endpoint}/${id}`);
+
   }
 
   getStudents():Promise<Student[]> {
-    return this.http.get(this.endpoint)
-      .toPromise()
-      .then(response => response.json() as Student[])
-      .catch(this.handleError);
+    return this.apiClient
+      .get<Student[]>(`${this.endpoint}`, StudentsService.handleError.bind(this));
   }
 
   saveStudent(student: Student): Promise<Student> {
     console.log('service:submit', student);
-    return this.http.put(`${this.endpoint}/${student.student_id}`, student)
-      .toPromise()
-      .then(response => response.json() as Student)
-      .catch(this.handleError)
+    return this.apiClient
+      .put<Student>(this.endpoint, +student.student_id, student);
   }
 
   createStudent(student: Student): Promise<Student> {
-    return this.http.post(`${this.endpoint}`, student)
-      .toPromise()
-      .then(response => response.json() as Student)
-      .catch(this.handleError)
+    return this.apiClient
+      .post<Student>(this.endpoint, student);
+  }
+
+  getGroups(): Promise<Group[]> {
+    return this.apiClient
+      .get<Group[]>('http://api.journal.ru/v1/group');
   }
 
 
 
-  handleError(err) {
+  static handleError(err) {
     console.error(err);
   }
 
