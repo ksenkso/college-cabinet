83189f073174a201e4f16aacc49f2ae22407893c
ksenkso <yazunya@yandex.ru>
2017-06-10 10:33:24 +0300

    [API] Client http methods optimized

--- src/app/modules/shared/services/api-client.service.ts
+++ src/app/modules/shared/services/api-client.service.ts
@@ -13,28 +13,7 @@ export class ApiClientService {
     private authService: AuthService
   ) { }
 
-  getFile(endpoint: string, headers: any = {}): Promise<Response> {
-    const requestHeaders: Headers = this.authService.getTokenAuthHeaders();
-
-    if (!requestHeaders.get('Authorization')) return;
-
-    for (let key in headers) {
-      if (headers.hasOwnProperty(key)) {
-        requestHeaders.append(key, headers[key]);
-      }
-    }
-
-    //requestHeaders.append('Content-Type', 'blob');
-
-    return this.http.get(this.apiURL + endpoint, {headers: requestHeaders, responseType: ResponseContentType.Blob})
-      .toPromise()
-      .then(response => {
-        return response;
-      })
-      .catch(ApiClientService.handleError);
-  }
-
-  get<T>(endpoint: string, headers: any = {}): Promise<T> {
+  private buildHeaders(headers: any): Promise<Headers> {
     const requestHeaders: Headers = this.authService.getTokenAuthHeaders();
 
     if (!requestHeaders.get('Authorization')) return Promise.reject(null);
@@ -45,64 +24,71 @@ export class ApiClientService {
       }
     }
 
-    return this.http.get(this.apiURL + endpoint, {headers: requestHeaders})
-      .toPromise()
-      .then(response => response.json() as T)
-      .catch(ApiClientService.handleError);
+    return Promise.resolve(requestHeaders)
+  }
+
+  getFile(endpoint: string, headers: any = {}): Promise<Response> {
+    return this.buildHeaders(headers)
+      .then((requestHeaders) => {
+        return this.http.get(this.apiURL + endpoint, {headers: requestHeaders, responseType: ResponseContentType.Blob})
+          .toPromise()
+          .then(response => {
+            return response;
+          })
+          .catch(ApiClientService.handleError);
+      })
+
+    //requestHeaders.append('Content-Type', 'blob');
+
+
+  }
+
+  get<T>(endpoint: string, headers: any = {}): Promise<T> {
+
+    return this.buildHeaders(headers)
+      .then((requestHeaders) => {
+        return this.http.get(this.apiURL + endpoint, {headers: requestHeaders})
+          .toPromise()
+          .then(response => response.json() as T)
+          .catch(ApiClientService.handleError);
+      });
   }
 
   post<T>(endpoint: string, body: any, headers: any = {}): Promise<T> {
-    const requestHeaders: Headers = this.authService.getTokenAuthHeaders();
 
-    if (!requestHeaders.get('Authorization')) return;
-
-    for (let key in headers) {
-      if (headers.hasOwnProperty(key)) {
-        requestHeaders.append(key, headers[key]);
-      }
-    }
-
-    return this.http
-      .post(this.apiURL + endpoint, body, {headers: requestHeaders})
-      .toPromise()
-      .then(response => response.json() as T)
-      .catch(ApiClientService.handleError);
+    return this.buildHeaders(headers)
+      .then((requestHeaders) => {
+        return this.http
+          .post(this.apiURL + endpoint, body, {headers: requestHeaders})
+          .toPromise()
+          .then(response => response.json() as T)
+          .catch(ApiClientService.handleError);
+      });
   }
 
   put<T>(endpoint: string, id: number, body: any, headers: any = {}): Promise<T> {
-    const requestHeaders: Headers = this.authService.getTokenAuthHeaders();
+    return this.buildHeaders(headers)
+      .then((requestHeaders) => {
+        return this.http
+          .put(`${this.apiURL + endpoint}/${id}`, body, {headers: requestHeaders})
+          .toPromise()
+          .then(response => response.json() as T)
+          .catch(ApiClientService.handleError);
+      })
 
-    if (!requestHeaders.get('Authorization')) return;
-
-    for (let key in headers) {
-      if (headers.hasOwnProperty(key)) {
-        requestHeaders.append(key, headers[key]);
-      }
-    }
-
-    return this.http
-      .put(`${this.apiURL + endpoint}/${id}`, body, {headers: requestHeaders})
-      .toPromise()
-      .then(response => response.json() as T)
-      .catch(ApiClientService.handleError);
   }
 
   remove<T>(endpoint: string, id: number | string, headers: any = {}): Promise<T> {
-    const requestHeaders: Headers = this.authService.getTokenAuthHeaders();
 
-    if (!requestHeaders.get('Authorization')) return;
+    return this.buildHeaders(headers)
+      .then((requestHeaders) => {
 
-    for (let key in headers) {
-      if (headers.hasOwnProperty(key)) {
-        requestHeaders.append(key, headers[key]);
-      }
-    }
-
-    return this.http
-      .delete(`${this.apiURL + endpoint}/${id}`, {headers: requestHeaders})
-      .toPromise()
-      .then(response => response.json() as T)
-      .catch(ApiClientService.handleError);
+        return this.http
+          .delete(`${this.apiURL + endpoint}/${id}`, {headers: requestHeaders})
+          .toPromise()
+          .then(response => response.json() as T)
+          .catch(ApiClientService.handleError);
+      });
   }
 
   static handleError(err: any) {
